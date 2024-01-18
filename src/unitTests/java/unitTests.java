164c5daba844b3b9b8db67ba6c012cb879ed2d27
Umit Tas <umittas2000@gmail.com>
2024-01-17 21:42:03 -0400

    2 more Unit tests added from hackerRank sample data

--- src/unitTests/java/unitTests.java
+++ src/unitTests/java/unitTests.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class unitTests {
 @Test
-    public void positive_DataSet1(){
+    public void positive_DataSet1_hackerRank1(){
         List<Integer> ranks = Arrays.asList(100,100,50,40,40,20,10);
         List<Integer> player = Arrays.asList(5,25,50,120);
         List<Integer> expectedResult = Arrays.asList(6,4,2,1);
@@ -56,5 +56,25 @@ public void positive_DataSet2(){
 
         List<Integer> actualResult = Main.rankedList(ranks,player);
         Assert.assertNotEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void positive_DataSet6_hackerRank2(){
+        List<Integer> ranks = Arrays.asList(100,90,90,80);
+        List<Integer> player = Arrays.asList(70,80,105);
+        List<Integer> expectedResult = Arrays.asList(4,3,1);
+
+        List<Integer> actualResult = Main.rankedList(ranks,player);
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void positive_DataSet7_hackerRank3(){
+        List<Integer> ranks = Arrays.asList(100,90,90,80,75,60);
+        List<Integer> player = Arrays.asList(50,65,77,90,102);
+        List<Integer> expectedResult = Arrays.asList(6,5,4,2,1);
+
+        List<Integer> actualResult = Main.rankedList(ranks,player);
+        Assert.assertEquals(expectedResult,actualResult);
     }
 }
